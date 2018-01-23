0178f261980d61424590ac9818a70f7e28039dd7
polo dev <polodev10@gmail.com>
2018-01-23 15:28:43 +0600

    added class 29

---
+++ index.php
@@ -0,0 +1,68 @@
+<?php
+#// encapsulation
+#// inheritance
+#// polymorphism
+#// overriding 
+#// overloading
+
+// class Abba {
+//   public $home = 'dhaka';
+//   public $name = 'Abba';
+// }
+// class Chele extends Abba {
+//   public $name = 'Chele';
+// }
+
+
+// $obj = new Chele;
+// $obj->eye = 2;
+// echo $obj->eye;
+
+// interface car {
+//   public function run();
+// }
+
+// interface airfreshener {
+//   public function spary ();
+// }
+
+// class Allien implements car, airfreshener {
+//   public function run() {
+//     echo 'we are running allien'; // definition
+//    }
+//    public function spary() {
+//     echo 'we are sparing air freshener';
+//    }
+// }
+
+// class Person {
+//   public $first_name;
+//   public $last_name;
+//   public function __construct ($first_name, $last_name) {
+//     $this->first_name = $first_name;
+//     $this->last_name = $last_name;
+//   }
+//   public $email;
+//   public function full_name () {
+//     return $this->first_name . $this->last_name;
+//   }
+// }
+
+// $obj = new Person('nur', 'uddin');
+// echo $obj->full_name();
+// class abba {
+//   const email = 'nur@gmail.com';
+// }
+
+// class AnotherPerson extends abba {
+//   const name = 'farhad';
+//   public static function hello () {
+//     echo self::name;
+//     echo parent::email;
+//   }
+// }
+
+// AnotherPerson::name;
+
+// echo AnotherPerson::hello();
+
---
+++ readme.md
@@ -0,0 +1,5 @@
+# `encapsulation`
+# `inheritance`
+# `__construct magic method`
+# `static properties and class`
+# `self, parent`
